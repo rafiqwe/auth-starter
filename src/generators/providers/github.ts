fff8c829eeaf69d80a2c64ec7ceec083f5fd5b36
rafiqwe <rmlmanik502@gmail.com>
2026-02-12 00:54:49 +0600

    Created: github provider generator

---
+++ src/generators/providers/github.ts
@@ -0,0 +1,33 @@
+import { ProviderModule } from "../../types/provider.js";
+
+export const GitHubProvider: ProviderModule = {
+  name: "github",
+
+  getImport() {
+    return `import GitHub from "next-auth/providers/github";`;
+  },
+
+  getConfig() {
+    return `
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    })`;
+  },
+
+  getEnv() {
+    return ["GITHUB_CLIENT_ID=", "GITHUB_CLIENT_SECRET="];
+  },
+
+  getDependencies() {
+    return [];
+  },
+};
