b2ab7384f13958091ebaf8c0afc4fa6107244400
rafiqwe <rmlmanik502@gmail.com>
2026-02-12 00:55:04 +0600

    Created: google provider generator

---
+++ src/generators/providers/google.ts
@@ -0,0 +1,33 @@
+import { ProviderModule } from "../../types/provider.js";
+
+export const GoogleProvider: ProviderModule = {
+  name: "google",
+
+  getImport() {
+    return `import Google from "next-auth/providers/google";`;
+  },
+
+  getConfig() {
+    return `
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    })`;
+  },
+
+  getEnv() {
+    return ["GOOGLE_CLIENT_ID=", "GOOGLE_CLIENT_SECRET="];
+  },
+
+  getDependencies() {
+    return [];
+  },
+};
