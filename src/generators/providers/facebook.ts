d20ee83d07c9757b1ad4fdfa4e120399ef4439d8
rafiqwe <rmlmanik502@gmail.com>
2026-02-12 00:54:28 +0600

    Created: Facebook generator

---
+++ src/generators/providers/facebook.ts
@@ -0,0 +1,33 @@
+import { ProviderModule } from "../../types/provider.js";
+
+export const FacebookProvider: ProviderModule = {
+  name: "facebook",
+
+  getImport() {
+    return `import Facebook from "next-auth/providers/facebook";`;
+  },
+
+  getConfig() {
+    return `
+    Facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    })`;
+  },
+
+  getEnv() {
+    return ["FACEBOOK_CLIENT_ID=", "FACEBOOK_CLIENT_SECRET="];
+  },
+
+  getDependencies() {
+    return [];
+  },
+};
