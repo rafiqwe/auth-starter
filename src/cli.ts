58906dbe30f228a91c0bf3577a53c28e5d0f73b0
rafiqwe <rmlmanik502@gmail.com>
2026-02-13 17:20:19 +0600

    Updated: cli.t

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 import { injectAuthConfig } from "./core/injectAuthConfig.js";
 import { runSetup } from "./engine/prompts/setup.js";
 import { injectMiddleware } from "./core/injectMiddleware.js";
@@ -13,10 +12,13 @@ import { prismaDeps } from "./generators/database/prisma/deps.js";
 import { prismaConfig } from "./generators/database/prisma/prismaConfig.js";
 import { injectUserType } from "./core/injectUserType.js";
 import chalk from "chalk";
+import { mongoDbConfig } from "./generators/database/mongodb/mongoDbConfig.js";
+import { mongodbDeps } from "./generators/database/mongodb/deps.js";
+
 // import { detectEnv } from "./engine/detect/env.js";
 
 async function main() {
-  intro("Welcome to Auth Starter");
+  intro("Welcome to AuthKit Pro");
   // const env = await detectEnv();
   const config = await runSetup();
   const targetDir = process.cwd();
@@ -26,7 +28,7 @@ async function main() {
   );
 
   // inject auth config
-  injectAuthConfig(config);
+  injectAuthConfig(config, config.database);
   if (ifCredentialProvider) {
     injectUserType();
   }
@@ -52,19 +54,66 @@ async function main() {
   );
 
   // if database is postgres or mongodb then generate prisma
-  if (config.database === "postgres" || config.database === "mongodb") {
+  if (config.database === "postgres") {
     generatePrisma(targetDir, config.database);
     // if database is postgres then generate prisma config
     if (config.database === "postgres") prismaConfig();
   }
 
+  if (config.database === "mongodb") {
+    mongoDbConfig();
+  }
+
   // generate env variables
-  generateEnv({ variables: envs });
+  generateEnv({ variables: envs, ans: config.database });
 
   // install dependencies
-  await installDeps({ deps: [...deps, ...prismaDeps()], ans: config.database });
+  const postgresDeps = config.database === "postgres" ? prismaDeps() : [];
+  const mongoDbDep = config.database === "mongodb" ? mongodbDeps() : [];
+
+  await installDeps({
+    deps: [...deps, ...postgresDeps, ...mongoDbDep],
+    ans: config.database,
+  });
+
+  console.log(chalk.cyan("Next steps:"));
+  console.log(chalk.yellow("1. Add your DATABASE_URL to the .env file"));
+  console.log(
+    chalk.yellow(
+      "2. Add your providers credentials CLIENT_ID and CLIENT_SECRET to the .env file",
+    ),
+  );
+
+  if (config.database === "postgres") {
+    console.log(
+      chalk.yellow("3. Run migrations: ") +
+        chalk.white.bold("npx prisma migrate dev --name init"),
+    );
+    console.log(
+      chalk.yellow("4. Generate Prisma client: ") +
+        chalk.white.bold("npx prisma generate"),
+    );
+    console.log(
+      chalk.yellow("5. Push schema to database: ") +
+        chalk.white.bold("npx prisma db push\n"),
+    );
+  }
+  if (config.database !== "none") {
+    console.log(
+      chalk.green(
+        `🎉 Your database and ${
+          config.database === "postgres" ? "Prisma" : "MongoDB"
+        } are ready! \n`,
+      ),
+    );
+  }
+  console.log(chalk.blue("Run npm run dev to start your application \n"));
+  console.log(
+    chalk.green("Login first then get: ") +
+      chalk.white.bold("localhost:3000/api/auth/check\n"),
+  );
   console.log(chalk.green(".env created"));
-  outro("Auth Starter completed");
+  outro("AuthKit Pro completed");
 }
 
 main();
