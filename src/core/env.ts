bc25764230f8e83d902b47ba4d108dd25a0c4eed
rafiqwe <rmlmanik502@gmail.com>
2026-02-14 22:18:47 +0600

    Updated: Env generator core

--- src/core/env.ts
+++ src/core/env.ts
@@ -1,14 +1,26 @@
 import fs from "fs";
 import crypto from "crypto";
-import chalk from "chalk";
 
-export function generateEnv({ variables }: { variables: string[] }) {
+export function generateEnv({
+  variables,
+  ans,
+}: {
+  variables: string[];
+  ans: string;
+}) {
   const secret = crypto.randomBytes(32).toString("hex");
+  let databaseUrl = "";
+  if (ans === "postgres") {
+    databaseUrl = "postgresql://USER:PASSWORD@localhost:5432/authkit-pro";
+  }
+  if (ans === "mongodb") {
+    databaseUrl = "mongodb://localhost:27017/authkit-pro";
+  }
 
   const content = `
 NEXTAUTH_SECRET=${secret}
 NEXTAUTH_URL=http://localhost:3000
-DATABASE_URL="postgresql://USER:PASSWORD@localhost:5432/db"
+DATABASE_URL=${databaseUrl}
 ${variables.map((v) => `${v}=`).join("\n")}
 `;
 
