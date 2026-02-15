ecd83ab91e6915048898d6510d4bc4504fc40bff
rafiqwe <rmlmanik502@gmail.com>
2026-02-15 23:15:06 +0600

    Updated: User profile UI

--- src/generators/ui/userProfile.ts
+++ src/generators/ui/userProfile.ts
@@ -1,32 +1,33 @@
 export function userProfile() {
-    return `
-"use client";
-
-import { useSession } from "next-auth/react";
+  return `
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 import Image from "next/image";
+import { SignOutButton } from "./SignOutButton";
 
-export function UserProfile() {
-  const { data } = useSession();
-
-  if (!data) return null;
-
+export default async function UserProfile() {
+  const session = await getServerSession(authOptions);
+  if (!session) return null;
   return (
-    <div className="flex items-center gap-3">
-      {data.user?.image && (
+    <div className="flex items-center gap-3 bg-amber-50 rounded-xl p-2 text-black">
+      {session.user?.image && (
         <Image
-          src={data.user.image}
-          alt=""
+          src={session.user.image}
+          alt="User Profile"
           width={40}
           height={40}
           className="rounded-full"
         />
       )}
       <div>
-        <p>{data.user?.name}</p>
-        <p className="text-sm opacity-70">{data.user?.email}</p>
+        <p className="font-medium">{session.user?.name}</p>
+        <p className="text-sm opacity-70">{session.user?.email}</p>
+      </div>
+      <div>
+        <SignOutButton />
       </div>
     </div>
   );
 }
 `;
-  }
+}
