081f95ed99196fad7a6dee43d879a2fe9447add5
rafiqwe <rmlmanik502@gmail.com>
2026-02-19 19:51:51 +0600

    Updated: install file

--- src/core/install.ts
+++ src/core/install.ts
@@ -1,24 +1,32 @@
 import { execa } from "execa";
 import chalk from "chalk";
-import { AuthCLIConfig } from "../types/config.js";
+import { Engine } from "../types/config.js";
 
 export async function installDeps({
   deps,
-  ans,
+  engine,
 }: {
   deps: string[];
-  ans: AuthCLIConfig["database"];
+  engine: Engine;
 }) {
-  const devDeps = ["next-auth", ...deps];
-  console.log(chalk.green("\nInstalling devDependencies:"));
-  devDeps.forEach((dep) => console.log(chalk.cyan("- " + dep)));
+  if (engine === "nextauth") {
+    const devDeps = ["next-auth", ...deps];
+    console.log(chalk.green("\nInstalling devDependencies:"));
+    devDeps.forEach((dep) => console.log(chalk.cyan("- " + dep)));
 
-  await execa("npm", ["install", "next-auth", ...deps], {
-    stdio: "inherit",
-  });
+    await execa("npm", ["install", "next-auth", ...deps], {
+      stdio: "inherit",
+    });
+  } else {
+    const devDeps = ["next-auth@beta", ...deps];
+    console.log(chalk.green("\nInstalling devDependencies:"));
+    devDeps.forEach((dep) => console.log(chalk.cyan("- " + dep)));
+
+    await execa("npm", ["install", "next-auth@beta", ...deps], {
+      stdio: "inherit",
+    });
+  }
   await execa("npm", ["install", "--save-dev", "@types/bcrypt"]);
 
   console.log(chalk.green.bold("\n✅ Dependencies installed successfully!\n"));
-
- 
 }
