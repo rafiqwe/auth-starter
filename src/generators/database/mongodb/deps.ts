d81a128beef108d29a80c07250e3316b7e7a32f2
rafiqwe <rmlmanik502@gmail.com>
2026-02-19 19:53:02 +0600

    Updated: Mongodb deps

--- src/generators/database/mongodb/deps.ts
+++ src/generators/database/mongodb/deps.ts
@@ -1,3 +1,9 @@
-export function mongodbDeps() {
-  return ["@next-auth/mongodb-adapter", "mongodb"];
+import { Engine } from "../../../types/config.js";
+
+export function mongodbDeps(engine: Engine) {
+  if (engine === "authjs") {
+    return ["@auth/mongodb-adapter", "mongodb"];
+  } else {
+    return ["@next-auth/mongodb-adapter", "mongodb"];
+  }
 }
